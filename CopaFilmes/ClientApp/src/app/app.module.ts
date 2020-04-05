a6eab72eb7465163899a372a9f598661749cfe19
vifelisberto <vi.felis@hotmail.com>
2020-04-04 23:17:08 -0300

    Iniciando página de resultado

--- CopaFilmes/ClientApp/src/app/app.module.ts
+++ CopaFilmes/ClientApp/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ResultadoComponent } from './resultado/resultado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ResultadoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,6 +26,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'resultado', component: ResultadoComponent }
     ])
   ],
   providers: [],
---
+++ CopaFilmes/ClientApp/src/app/resultado/resultado.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado',
+  templateUrl: './resultado.component.html',
+  styleUrls: ['./resultado.component.css']
+})
+export class ResultadoComponent {
+}
